1021a171470b9f796d9c20fb0efebeebd3afc6f8
Weslley Thiago <weslleyothiago@gmail.com>
2024-10-19 16:20:29 -0300

    Dotenv funcionando

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+require('dotenv').config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
 
   // Pipes
   app.useGlobalPipes(
